9cde1fe6a9c1cc48ae88405fcd62301cfa31426d
Piotr Patrzyk <piotr@patrzyk.me>
2022-05-09 17:54:46 +0200

    module docs fix

--- merkury/main.py
+++ merkury/main.py
@@ -1,16 +1,18 @@
 
 """merkury
+
 Usage:
     merkury <file>
+
 Options:
-    -h --help        Show this screen.
+    -h --help     Show this screen.
+    --version     Show version.
 """
 
 from docopt import docopt
 
 def main():
-    args= docopt(__doc__)
-    print('main called')
+    args = docopt(__doc__, version="merkury 0.1")
     print(args)
 
 if __name__ == "__main__":
