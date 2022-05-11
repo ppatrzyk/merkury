7dc30a8ecce71d1d65629938e869ea40c9e7358f
Piotr Patrzyk <piotr@patrzyk.me>
2022-05-11 12:51:39 +0200

    style fixes

--- merkury/renderer.py
+++ merkury/renderer.py
@@ -16,13 +16,10 @@ def join_chunks(code_inputs, code_outputs):
     in_chunk = out_chunk = ""
     for input, output in zip(code_inputs, code_outputs):
         in_chunk += "".join((line+"\n" for line in input))
-        match output:
-            case '':
-                pass
-            case _:
-                out_chunk += output
-                yield {"in": in_chunk, "out": out_chunk}
-                in_chunk = out_chunk = ""
+        if output != '':
+            out_chunk += output
+            yield {"in": in_chunk, "out": out_chunk}
+            in_chunk = out_chunk = ""
 
 def generate_template(chunks):
     """
