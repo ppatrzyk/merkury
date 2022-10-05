df6f3e6ecb730b89e54df2266f715e8b74abda89
Piotr Patrzyk <piotr@patrzyk.me>
2022-10-05 21:15:19 +0200

    file name generation fix

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,3 +6,5 @@ def test_get_default_path():
     assert isinstance(get_default_path(pathlib.Path("abs/path/file.py"), "html"), pathlib.Path)
     assert re.search(r"^/abs/path/file_.*\.html$", str(get_default_path(pathlib.Path("/abs/path/file.py"), "html")))
     assert re.search(r"^rel/file_.*\.pdf$", str(get_default_path(pathlib.Path("rel/file.py"), "pdf")))
+    assert re.search(r"^rel/file_.*\.pdf$", str(get_default_path(pathlib.Path("rel/file.sql"), "pdf")))
+    assert re.search(r"^rel/filesql_.*\.html$", str(get_default_path(pathlib.Path("rel/filesql"), "html")))
