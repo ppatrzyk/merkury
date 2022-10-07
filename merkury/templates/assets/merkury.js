847cbe31d991d7fac2740e56d61a58e2f2932f4c
Piotr Patrzyk <piotr@patrzyk.me>
2022-10-07 18:41:59 +0200

    listjs init

--- merkury/templates/assets/merkury.js
+++ merkury/templates/assets/merkury.js
@@ -13,3 +13,22 @@ function change_theme() {
         prism_light.setAttribute("media", "max-width: 1px")
     }
 }
+
+function interactive_tables() {
+    console.log("interactive_tables called");
+    var tables = Array.from(document.getElementsByTagName("table"));
+    tables.forEach(table => {
+        // todo tryexcept that
+        table.insertAdjacentHTML("beforebegin", '<input class="search" placeholder="Search" />');
+        var body = table.getElementsByTagName("tbody")[0];
+        body.classList.add('list');
+        var options = {
+        valueNames: [ 'name', 'born' ]
+        };
+        var table_listjs = new List('users', options);
+        // todo all column heads into sort buttons
+        // https://listjs.com/examples/table/
+    });
+}
+
+document.addEventListener("DOMContentLoaded", interactive_tables);
