cb8bb712d596f71a0c76b1b363c06edf99b0ee40
Piotr Patrzyk <piotr@patrzyk.me>
2022-10-10 18:02:53 +0200

    rerun examples

--- examples/intro-sql.sql
+++ examples/intro-sql.sql
@@ -1,12 +1,12 @@
 -- Script ran against Chinook database
 -- https://github.com/lerocha/chinook-database
 
-select BillingCountry, count(*) as Invoices, sum(Total) as Total_spent from Invoice
-group by BillingCountry
-order by Invoices desc;
-
 select Name, count(*) as Albums from Album
 left join Artist using (ArtistId)
 group by ArtistId
 having Albums > 1
 order by Albums desc;
+
+select BillingCountry as Country, count(distinct(CustomerId)) as Customers, count(*) as Invoices from Invoice
+group by BillingCountry
+order by Invoices desc;
