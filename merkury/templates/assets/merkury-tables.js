c7d839ed78369b2be020c20cac62031e7c2a0537
Piotr Patrzyk <piotr@patrzyk.me>
2022-10-10 16:46:18 +0200

    tables styling

--- merkury/templates/assets/merkury-tables.js
+++ merkury/templates/assets/merkury-tables.js
@@ -6,16 +6,12 @@ function make_table(table) {
     var headers = Array.from(table.getElementsByTagName("th"));
     header_classes = headers.map(header => `${header.textContent.replace(/\s/g, "")}_${random_str()}`);
     headers.forEach((header, i, _headers) => {
-        var sort_button = document.createElement("button");
+        header.innerText += " ";
+        var sort_button = document.createElement("a");
         sort_button.classList.add("sort");
         sort_button.setAttribute("data-sort", header_classes[i]);
-        sort_button.innerText = header.innerText;
-        header.innerText = "";
-        // TODO think about best format of this
-        // Either adjust button style or put next to text as "sort"?
-        // sort_button.innerText = "sort";
+        sort_button.innerHTML = "&#8645;";
         header.appendChild(sort_button);
-        // header.replaceWith(sort_button);
     });
     var cells = Array.from(table.getElementsByTagName("td"));
     cells.forEach((cell, i, _cells) => {
@@ -25,16 +21,18 @@ function make_table(table) {
     var div_parent = document.createElement("div");
     div_parent.id = random_str();
     current_parent.replaceChild(div_parent, table);
-    search_input = document.createElement("input")
-    search_input.classList.add("search")
-    search_input.setAttribute("placeholder", "Search")
-    // TODO pagination better style or remove
-    pagination = document.createElement("ul")
-    pagination.classList.add("pagination")
+    search_input = document.createElement("input");
+    search_input.classList.add("search");
+    search_input.setAttribute("placeholder", "Search");
+    pagination_head = document.createElement("div");
+    pagination_head.innerText = "Pages";
+    pagination = document.createElement("ul");
+    pagination.classList.add("pagination");
     div_parent.appendChild(search_input);
     div_parent.appendChild(table);
+    div_parent.appendChild(pagination_head);
     div_parent.appendChild(pagination);
-    var list_js_opts = {valueNames: header_classes, page: 10, pagination: true}
+    var list_js_opts = {valueNames: header_classes, page: 10, pagination: true};
     var table_listjs = new List(div_parent.id, list_js_opts);
 }
 
