748670bf54ec00a541026bc12e057589b06b5c66
Piotr Patrzyk <piotr@patrzyk.me>
2023-01-09 12:15:58 +0100

    take title as arg

--- merkury/main.py
+++ merkury/main.py
@@ -11,6 +11,7 @@ Options:
     -f <format>, --format <format>  Specify report format: html (default), md.
     -i, --interactive               Make tables interactive (search, sort, paging). Valid for HTML output.
     -a <author>, --author <author>  Specify author (if missing, user name).
+    -t <title>, --title <title>
     -v, --version                   Show version and exit.
 """
 
@@ -36,6 +37,7 @@ def main():
     format = (args.get("--format") or "html").lower()
     assert format in FORMATS, f"Unknown format: {format}. Options: html, md"
     script_file_path = Path(args.get("<script>"))
+    file_name = script_file_path.name
     start = time()
     match script_file_path.suffix.lower():
         case ".py":
@@ -54,7 +56,8 @@ def main():
         "interactive": bool(args.get("--interactive")),
         "author": (args.get("--author") or getlogin()),
         "script_type": script_type,
-        "file_name": script_file_path.name,
+        "title": args.get("--title") or file_name,
+        "file_name": file_name,
         "report_file_path": Path(args.get("--output") or get_default_path(script_file_path, format)),
         "timestamp": datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %Z"),
         "version": VERSION,
